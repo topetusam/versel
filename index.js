196d0aaa7620bcfca82f47de4ae2f8900fd72e42
topetusam <67940360+topetusam@users.noreply.github.com>
2024-10-29 08:35:09 -0500

    feat: :sparkles: configuraciones necesarioas versel

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// index.js
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 // Datos de ejemplo
@@ -13,47 +10,15 @@ let usuarios = [
 ];
 
 // Rutas
-
-// Obtener todos los usuarios
 app.get("/api/usuarios", (req, res) => {
   res.json(usuarios);
 });
 
-// Obtener un usuario por ID
-app.get("/api/usuarios/:id", (req, res) => {
-  const usuario = usuarios.find((u) => u.id === parseInt(req.params.id));
-  if (usuario) res.json(usuario);
-  else res.status(404).json({ mensaje: "Usuario no encontrado" });
+// Puerto de escucha
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor en ejecución en el puerto ${PORT}`);
 });
 
-// Crear un nuevo usuario
-app.post("/api/usuarios", (req, res) => {
-  const nuevoUsuario = { id: usuarios.length + 1, ...req.body };
-  usuarios.push(nuevoUsuario);
-  res.status(201).json(nuevoUsuario);
-});
 
-// Actualizar un usuario por ID
-app.put("/api/usuarios/:id", (req, res) => {
-  const usuario = usuarios.find((u) => u.id === parseInt(req.params.id));
-  if (usuario) {
-    Object.assign(usuario, req.body);
-    res.json(usuario);
-  } else res.status(404).json({ mensaje: "Usuario no encontrado" });
-});
 
-// Eliminar un usuario por ID
-app.delete("/api/usuarios/:id", (req, res) => {
-  const index = usuarios.findIndex((u) => u.id === parseInt(req.params.id));
-  if (index !== -1) {
-    usuarios.splice(index, 1);
-    res.status(204).end();
-  } else res.status(404).json({ mensaje: "Usuario no encontrado" });
-});
-
-// // Puerto de escucha (necesario para Vercel)
-// app.listen(3000, () => {
-//   console.log("Servidor en ejecución en el puerto 3000");
-// });
-
-module.exports = app;
